0d18ea7a074dc9f6d81b997ae8a977e5b4c46071
adibakshi <adibakshi28@gmail.com>
2025-01-04 13:29:03 -0500

    revert back to safe state

--- app/jobs/scheduler.py
+++ app/jobs/scheduler.py
@@ -1,12 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.interval import IntervalTrigger
-from app.jobs.tasks import update_stock_universe_cache, sync_stock_universe, another_task, print_stock_subscription_table, fe_be_websocket_msg_broadcast, populate_stock_universe
+from app.jobs.tasks import update_stock_universe_cache, sync_stock_universe, another_task, print_stock_subscription_table, fe_be_websocket_msg_broadcast
 from app.core.config import config
 import asyncio
 import atexit
 
-number = 0
+# Note: APScheduler operates in a separate thread from the FastAPI event loop
 
 def run_async_job(async_func):
     loop = asyncio.new_event_loop()
@@ -20,7 +20,15 @@ def run_async_job(async_func):
 def start_scheduler():
     scheduler = BackgroundScheduler()
 
-    scheduler.add_job(run_async_job, IntervalTrigger(seconds=1.15), id="populate_stock_universe", replace_existing=True, args=[populate_stock_universe])
+    # Add periodic jobs (non async functions)
+    scheduler.add_job(another_task, IntervalTrigger(minutes=30), id="another_task", replace_existing=True)
+    scheduler.add_job(sync_stock_universe, CronTrigger(hour=1, minute=0, timezone=config["TIMEZONE"]), id="sync_stock_universe", replace_existing=True)
+    
+    # For running async functions
+    scheduler.add_job(run_async_job, IntervalTrigger(hours=24), id="update_stock_universe_cache", replace_existing=True, args=[update_stock_universe_cache])
+    scheduler.add_job(run_async_job, IntervalTrigger(seconds=config['FE_BE_WEBSOCKET_MSG_FREQUENCY']), id="fe_be_websocket_msg_broadcast", replace_existing=True, args=[fe_be_websocket_msg_broadcast])
+
+    # scheduler.add_job(run_async_job, IntervalTrigger(seconds=15), id="print_stock_subscription_table_task", replace_existing=True, args=[print_stock_subscription_table])
 
     scheduler.start()
 
